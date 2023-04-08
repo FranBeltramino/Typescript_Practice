3f46b231160cacda815e49e9060a8b96b8208d67
FranBeltramino <franbeltra16@gmail.com>
2023-04-08 19:54:48 -0300

    firstUpdate

---
+++ dataTypes.js
@@ -0,0 +1,11 @@
+var name1 = 'Francisco';
+console.log(name1);
+var condition = true;
+console.log(condition);
+var numberA = 24;
+console.log(numberA);
+var numberB = [1, 2, 3, 4, 5];
+//or
+var names = ['Francisco', 'Juan', 'Carlos'];
+console.log(numberB);
+console.log(names);
---
+++ dataTypes.ts
@@ -0,0 +1,19 @@
+let name1:string = 'Francisco'; //STRING :)
+console.log(name1);
+
+let condition:boolean = true; //BOOL
+console.log(condition);
+
+let numberA:number = 24; //NUMBER
+console.log(numberA)
+
+let numberB:number[] = [1,2,3,4,5];  //ARRAY {FORM 1}
+//or
+let names: Array<string> = ['Francisco', 'Juan', 'Carlos']; //ARRAY {FORM 2}
+
+console.log(numberB);
+console.log(names); 
+
+let tuple1: [string, number, boolean] = ['haha', 35, false]; //TUPLE
+console.log(tuple1);
+console.log(tuple1[2]); // 0,1,2
