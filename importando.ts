6830629f99aad35aa4c07b37e4f4bcce8882b0f1
FranBeltramino <franbeltra16@gmail.com>
2023-04-11 09:56:53 -0300

    more updates

---
+++ importando.ts
@@ -0,0 +1,8 @@
+import { nombreG } from "./modulos";   //Exportando del archivo modulos la variable nombreG
+
+console.log(nombreG) //Se importa en terminal
+
+
+import { sumando } from "./suma"; //Importamos funcion flecha del archivo suma.ts
+
+console.log(sumando(4,4))
---
+++ suma.ts
@@ -0,0 +1,3 @@
+export const sumando = (a:number, b:number):number => {
+    return a + b;
+}
